b106499a34d86c6a4952807f832d704681e415f7
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-15 20:56:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet (planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
- calls.all? { |call| call.length >= 4 }
+ calls.any? { |call| call.length > 4 }
 end
 
 def find_the_cheese(cheese_types)
