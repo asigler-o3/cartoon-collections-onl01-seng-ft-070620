e01142cca01e542bf0501d32477d565c80a20b33
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-15 20:52:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ def summon_captain_planet (planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  calls.any? { |call| call.length > 4 }
+  calls.any? { |call| call.length < 4 }
   end
   calls.all? { |call| call.length >= 4 }
 end
